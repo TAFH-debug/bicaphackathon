99f5de0b1b093cbae964d919e50d25c5ea5e984e
TAFH-debug <aushahman2007@gmail.com>
2024-11-08 23:22:55 +0500

    eeeeee

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,23 +5,21 @@ export function Sidebar({ children }: { children: React.ReactNode }) {
     return (
         <div>
             <div className="w-full flex flex-col md:flex-row">
-                {/* Sidebar on larger screens, horizontal nav on small screens */}
                 <div className="flex md:flex-col w-full md:w-1/6 space-x-2 md:space-x-0 md:space-y-2">
                     <Button className="w-full mx-1" as={Link} href="/dashboard" variant="light">
                         <AiOutlineDashboard /><span>Dashboard</span>
                     </Button>
                     <Button className="w-full mx-1" as={Link} href="/kaz" variant="light">
-                        <AiOutlineGroup /><span>Bookshelfs</span>
+                        <AiOutlineGroup /><span>Казахский язык</span>
                     </Button>
                     <Button className="w-full mx-1" as={Link} href="/math" variant="light">
-                        <AiOutlineComment /><span>Clubs</span>
+                        <AiOutlineComment /><span>Математика</span>
                     </Button>
                     <Button className="w-full mx-1" as={Link} href="/history" variant="light">
-                        <AiOutlineBook /><span>Books</span>
+                        <AiOutlineBook /><span>История</span>
                     </Button>
                 </div>
 
-                {/* Content Area */}
                 <div className="w-full md:w-5/6">
                     {children}
                 </div>
